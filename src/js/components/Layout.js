1712440aca46dc82a32261a9e8ac06028600ba34
Maiko1100 <maiko1100@gmail.com>
2017-01-19 23:20:31 +0100

    paar changes

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -2,42 +2,33 @@ import React from "react"
 import { connect } from "react-redux"
 
 import { fetchUsers } from "../actions/userActions"
-import { fetchTweets } from "../actions/tweetsActions"
 
 @connect((store) => {
   return {
     users: store.users.users,
-    tweets: store.tweets.tweets,
   };
 })
 export default class Layout extends React.Component {
+
   componentWillMount() {
+
   }
 
-  fetchTweets() {
-    this.props.dispatch(fetchTweets())
+  fetchUsers() {
+    this.props.dispatch(fetchUsers())
   }
-    fetchTweets() {
-        this.props.dispatch(fetchUsers())
-    }
 
   render() {
-    const { users, tweets } = this.props;
+    const { users } = this.props;
 
-    // if (!tweets.length) {
-    //   return <button onClick={this.fetchTweets.bind(this)}>load tweets</button>
-    // }
-      if (!users.length) {
-          return <button onClick={this.fetchTweets.bind(this)}>load tweets</button>
-      }
+    const mappedUsers = users.map(user => <li key={user.id}>{user.name}</li>)
 
-    const mappedTweets = tweets.map(tweet => <li>{tweet.text}</li>)
 
-      const mappedUsers = users.map(user => <li>{user.name}</li>)
-        console.log(mappedUsers)
     return <div>
-        <ul>{mappedUsers}</ul>
-      <ul>{mappedTweets}</ul>
+
+      <button onClick={this.fetchUsers.bind(this)}>load users</button>
+
+      <ul>{mappedUsers}</ul>
     </div>
   }
 }
